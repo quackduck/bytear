f9a8ce0378b32be9ae264b11f3c9d5be2ca6ceb0
Ishan Goel <igoel.mail@gmail.com>
2023-10-21 05:15:44 -0400

    multi-byte chunks for real

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main //schlobbin, shlobbers
 
 import (
+	"io"
+	"math/big"
 	"os"
 	"strconv"
 	"time"
 
 	"github.com/faiface/beep"
 	"github.com/faiface/beep/speaker"
-	"github.com/quackduck/aces"
 )
 
 // curl "https://releases.ubuntu.com/22.04.3/ubuntu-22.04.3-desktop-amd64.iso" | ./bytear 8
@@ -21,23 +22,25 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	reader, err := aces.NewBitReader(uint8(chunklen), os.Stdin)
-	if err != nil {
-		panic(err)
-	}
+	//reader, err := aces.NewBitReader(uint8(chunklen), os.Stdin)
+	//if err != nil {
+	//	panic(err)
+	//}
 	sr := beep.SampleRate(sampleRate) // 44100
 	speaker.Init(sr, sr.N(time.Second/10))
 	println("now playing")
 	doneChan := make(chan bool, 1)
-	speaker.Play(audio(reader, uint8(chunklen), doneChan))
+	speaker.Play(audio(os.Stdin, int64(chunklen), doneChan))
 	<-doneChan
 }
 
-func audio(reader *aces.BitReader, chunkLen uint8, done chan bool) beep.Streamer {
-	r := 1 << chunkLen
+func audio(reader io.Reader, chunkLen int64, done chan bool) beep.Streamer {
+	buf := make([]byte, chunkLen)
+	num := big.NewInt(0)
+	r := big.NewInt(0).Exp(big.NewInt(2), big.NewInt(chunkLen*8), nil)
 	return beep.StreamerFunc(func(samples [][2]float64) (n int, ok bool) {
 		for i := range samples {
-			b, err := reader.Read()
+			_, err := io.ReadFull(reader, buf)
 			if err != nil {
 				if err.Error() == "EOF" {
 					done <- true
@@ -45,7 +48,11 @@ func audio(reader *aces.BitReader, chunkLen uint8, done chan bool) beep.Streamer
 				}
 				panic(err)
 			}
-			samples[i][0] = float64(b) / float64(r)
+
+			num.SetBytes(buf)
+			samples[i][0], _ = new(big.Rat).SetFrac(num, r).Float64()
+			samples[i][0] = samples[i][0]*2 - 1
+			//fmt.Println(samples[i][0])
 			samples[i][1] = samples[i][0]
 		}
 		return len(samples), true
