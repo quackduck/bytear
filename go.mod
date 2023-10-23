4432fc24d0fe44332d85505d4b4b5658fab5de2a
Ishan Goel <igoel.mail@gmail.com>
2023-10-23 03:43:18 -0400

    write to wav file

--- go.mod
+++ go.mod
@@ -3,16 +3,18 @@ module bytear
 go 1.21.3
 
 require (
-	github.com/crazy3lf/colorconv v1.2.0
 	github.com/faiface/beep v1.1.0
-	github.com/quackduck/aces v1.2.1
+	github.com/youpy/go-wav v0.3.2
 )
 
 require (
-	github.com/hajimehoshi/oto v0.7.1 // indirect
+	github.com/hajimehoshi/oto v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/youpy/go-riff v0.1.0 // indirect
+	github.com/zaf/g711 v0.0.0-20220109202201-cf0017bf0359 // indirect
 	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
+	golang.org/x/exp/shiny v0.0.0-20231006140011-7918f672742d // indirect
 	golang.org/x/image v0.13.0 // indirect
-	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
-	golang.org/x/sys v0.0.0-20190626150813-e07cf5db2756 // indirect
+	golang.org/x/mobile v0.0.0-20231006135142-2b44d11868fe // indirect
+	golang.org/x/sys v0.13.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main //schlobbin, shlobbers
 
 import (
+	"bufio"
 	"io"
 	"math/big"
 	"os"
@@ -9,19 +10,31 @@ import (
 
 	"github.com/faiface/beep"
 	"github.com/faiface/beep/speaker"
+	"github.com/youpy/go-wav"
 )
 
 // curl "https://releases.ubuntu.com/22.04.3/ubuntu-22.04.3-desktop-amd64.iso" | ./bytear 8
 
+// const sampleRate = 640000 //44100 / 2
+//const sampleRate = 1000
+
 const sampleRate = 44100 / 2
 
 func main() {
-	chunklenS := os.Args[1]
-
-	chunklen, err := strconv.Atoi(chunklenS)
+	file, err := os.Create("out.wav")
 	if err != nil {
 		panic(err)
 	}
+	chunkLenS := os.Args[1]
+
+	chunkLen, err := strconv.Atoi(chunkLenS)
+	if err != nil {
+		panic(err)
+	}
+	writeWAV(bufio.NewReaderSize(os.Stdin, 1024*1024), int64(chunkLen), file, 60*3, sampleRate)
+	return
+
+	writer := wav.NewWriter(file, sampleRate*60, 1, sampleRate, uint16(chunkLen*8))
 	//reader, err := aces.NewBitReader(uint8(chunklen), os.Stdin)
 	//if err != nil {
 	//	panic(err)
@@ -30,19 +43,51 @@ func main() {
 	speaker.Init(sr, sr.N(time.Second/10))
 	println("now playing")
 	doneChan := make(chan bool, 1)
-	speaker.Play(audio(os.Stdin, int64(chunklen), doneChan))
+	speaker.Play(audio(os.Stdin, int64(chunkLen), doneChan, writer))
 	<-doneChan
 }
 
-func audio(reader io.Reader, chunkLen int64, done chan bool) beep.Streamer {
+func writeWAV(reader io.Reader, chunkLen int64, writer io.Writer, seconds int, sampleRate int) {
+	buf := make([]byte, chunkLen)
+	num := big.NewInt(0)
+	//r := big.NewInt(0).Exp(big.NewInt(2), big.NewInt(chunkLen*8), nil)
+	w := wav.NewWriter(writer, uint32(sampleRate*seconds), 1, uint32(sampleRate), uint16(chunkLen*8))
+	s := make([]wav.Sample, seconds*sampleRate)
+	for i := range s {
+		_, err := io.ReadFull(reader, buf)
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+				//return
+			}
+			panic(err)
+		}
+		num.SetBytes(buf)
+		s[i] = wav.Sample{Values: [2]int{int(num.Int64()), int(num.Int64())}}
+		//if i%(sampleRate*30) == 0 {
+		//	fmt.Println(float64(i) / float64(seconds*sampleRate))
+		//}
+	}
+	err := w.WriteSamples(s)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func audio(reader io.Reader, chunkLen int64, done chan bool, writer *wav.Writer) beep.Streamer {
 	buf := make([]byte, chunkLen)
 	num := big.NewInt(0)
 	r := big.NewInt(0).Exp(big.NewInt(2), big.NewInt(chunkLen*8), nil)
 	return beep.StreamerFunc(func(samples [][2]float64) (n int, ok bool) {
+		s := make([]wav.Sample, len(samples))
 		for i := range samples {
 			_, err := io.ReadFull(reader, buf)
 			if err != nil {
 				if err.Error() == "EOF" {
+					err = writer.WriteSamples(s)
+					if err != nil {
+						panic(err)
+					}
 					done <- true
 					return len(samples), false
 				}
@@ -50,10 +95,18 @@ func audio(reader io.Reader, chunkLen int64, done chan bool) beep.Streamer {
 			}
 
 			num.SetBytes(buf)
+			s[i].Values[0] = int(num.Int64())
+			s[i].Values[1] = int(num.Int64())
+
 			samples[i][0], _ = new(big.Rat).SetFrac(num, r).Float64()
 			samples[i][0] = samples[i][0]*2 - 1
 			//fmt.Println(samples[i][0])
 			samples[i][1] = samples[i][0]
+		}
+		//fmt.Println(s)
+		err := writer.WriteSamples(s)
+		if err != nil {
+			panic(err)
 		}
 		return len(samples), true
 	})
